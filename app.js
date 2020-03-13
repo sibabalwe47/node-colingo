92af572c01efde363cae7e44c560e95907a14417
Sibabalwe Ngqoyiya <siba@thisisembrace.com>
2020-03-13 14:58:22 +0200

    port changes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv')
+
+dotenv.config({path: './config/config.env'})
 
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('GET ROUTE')
